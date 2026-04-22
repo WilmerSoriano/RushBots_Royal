90a6dc357f7ea877ab2548bd11a270bdc37c6c1a
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-04-22 00:03:10 -0500

    Added my Menu section java

--- royalgame/src/main/java/com/rushbot/App.java
+++ royalgame/src/main/java/com/rushbot/App.java
@@ -1,12 +1,12 @@
 package com.rushbot;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -16,8 +16,8 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws IOException {        
+        scene = new Scene(loadFXML("MenuScene"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
