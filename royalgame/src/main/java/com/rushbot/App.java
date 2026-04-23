60b1a4ce2926fd56b15ca3cd1deb878d0311d794
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-04-22 19:37:32 -0500

    Added CSS sheet, might take a while to find creative arts into program

--- royalgame/src/main/java/com/rushbot/App.java
+++ royalgame/src/main/java/com/rushbot/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {        
-        scene = new Scene(loadFXML("MenuScene"), 640, 480);
+        scene = new Scene(loadFXML("MenuScene"));
+        scene.getStylesheets().add(App.class.getResource("css/application.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
